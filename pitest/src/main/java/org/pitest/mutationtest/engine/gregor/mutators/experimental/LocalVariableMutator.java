ea7f3c1020a16697e84150b59d919d82ee91a450
Ayaan Kazerouni <ayaan@vt.edu>
2019-07-28 21:20:11 -0400

    First pass at local variable deletion

--- pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/experimental/LocalVariableMutator.java
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/experimental/LocalVariableMutator.java
@@ -16,26 +16,52 @@ public class LocalVariableMutator implements MethodMutatorFactory {
         public LocalVariableVisitor(final MutationContext context, final MethodVisitor delegateVisitor) { super(ASMVersion.ASM_VERSION, delegateVisitor);
             this.context = context;
         }
-        
+
         @Override
         public void visitVarInsn(final int opcode, int var) {
-            if (Opcodes.ISTORE == opcode && this.shouldMutate(var)) {
-                super.visitInsn(Opcodes.POP);
-                super.visitInsn(Opcodes.ICONST_0);
+            if (this.shouldMutate(opcode, var)) {
+                switch (opcode) {
+                    case Opcodes.ISTORE:
+                        super.visitInsn(Opcodes.POP);
+                        super.visitInsn(Opcodes.ICONST_0);
+                        break;
+                    case Opcodes.FSTORE:
+                        super.visitInsn(Opcodes.POP);
+                        super.visitInsn(Opcodes.FCONST_0);
+                        break;
+                    case Opcodes.DSTORE:
+                        super.visitInsn(Opcodes.POP2);
+                        super.visitInsn(Opcodes.DCONST_0);
+                        break;
+                    case Opcodes.LSTORE:
+                        super.visitInsn(Opcodes.POP2);
+                        super.visitInsn(Opcodes.LCONST_0);
+                        break;
+                    case Opcodes.ASTORE:
+                        super.visitInsn(Opcodes.POP);
+                        super.visitInsn(Opcodes.ACONST_NULL);
+                        break;
+                }
             }
             super.visitVarInsn(opcode, var);
         }
-        
+
         @Override
         public void visitMethodInsn(final int opcode, final String owner,
                 final String name, final String desc, boolean itf) {
             super.visitMethodInsn(opcode, owner, name, desc, itf);
         }
-        
-        private boolean shouldMutate(final int var) {
-            final MutationIdentifier mutationId = this.context.registerMutation(
-                    LocalVariableMutator.this, "Removed assignment to local variable " + var);
-            return this.context.shouldMutate(mutationId);
+
+        private boolean shouldMutate(final int opcode, final int var) {
+            if (opcode == Opcodes.ISTORE || opcode == Opcodes.FSTORE ||
+                opcode == Opcodes.DSTORE || opcode == Opcodes.LSTORE ||
+                opcode == Opcodes.ASTORE) {
+                final MutationIdentifier mutationId = this.context.registerMutation(
+                        LocalVariableMutator.this, "Removed assignment to local variable " + var);
+                return this.context.shouldMutate(mutationId);
+            }
+
+            return false;
         }
     }
 
--- pitest/src/test/java/org/pitest/mutationtest/engine/gregor/mutators/experimental/LocalVariableMutatorTest.java
+++ pitest/src/test/java/org/pitest/mutationtest/engine/gregor/mutators/experimental/LocalVariableMutatorTest.java
@@ -12,8 +12,21 @@ public class LocalVariableMutatorTest extends MutatorTestBase {
     public void setupEngineToMutateOnlyLocalVariables() {
       createTesteeWith(new LocalVariableMutator());
     }
-    
-    private static class HasLocalVariable implements Callable<Object> {
+
+
+    private static class HasNoLocals implements Callable<Object> {
+        @Override
+        public Integer call() throws Exception {
+            return 10;
+        }
+    }
+
+    @Test
+    public void shouldHaveNoMutants() throws Exception {
+        assertNoMutants(HasNoLocals.class);
+    }
+
+    private static class HasLocalInteger implements Callable<Object> {
         @Override
         public Integer call() throws Exception {
             int a = 1;
@@ -23,11 +36,54 @@ public class LocalVariableMutatorTest extends MutatorTestBase {
     
     @Test
     public void shouldReplaceIntAssignment1With0() throws Exception {
-        final Mutant mutant = getFirstMutant(HasLocalVariable.class);
-        assertMutantCallableReturns(new HasLocalVariable(), mutant, 0);
+        final Mutant mutant = getFirstMutant(HasLocalInteger.class);
+        assertMutantCallableReturns(new HasLocalInteger(), mutant, 0);
     }
-    
-    private static class HasLocalVariableWithTwoAssignments implements Callable<Object> {
+
+    private static class HasLocalBoolean implements Callable<Object> {
+        @Override
+        public Boolean call() throws Exception {
+            boolean a = true;
+            return a;
+        }
+    }
+
+    @Test
+    public void shouldReplaceBooleanAssignmentTrueWithFalse() throws Exception {
+        final Mutant mutant = getFirstMutant(HasLocalBoolean.class);
+        assertMutantCallableReturns(new HasLocalBoolean(), mutant, false);
+    }
+
+    private static class HasLocalChar implements Callable<Object> {
+        @Override
+        public Character call() throws Exception {
+            char a = 'a';
+            return a;
+        }
+    }
+
+    @Test
+    public void shouldReplaceCharacterAssignmentWith0() throws Exception {
+        final Mutant mutant = getFirstMutant(HasLocalChar.class);
+        assertMutantCallableReturns(new HasLocalChar(), mutant, '\u0000');
+    }
+
+
+    private static class HasLocalByte implements Callable<Object> {
+        @Override
+        public Byte call() throws Exception {
+            byte a = 1;
+            return a;
+        }
+    }
+
+    @Test
+    public void shouldReplaceByteAssignmentWith0() throws Exception {
+        final Mutant mutant = getFirstMutant(HasLocalByte.class);
+        assertMutantCallableReturns(new HasLocalByte(), mutant, (byte) 0);
+    }
+
+    private static class HasLocalVariableWithSecondAssignment implements Callable<Object> {
         @Override
         public Integer call() throws Exception {
             int a = 1;
@@ -35,10 +91,71 @@ public class LocalVariableMutatorTest extends MutatorTestBase {
             return a;
         }
     }
-    
+
     @Test
-    public void shouldReplaceTwoIntAssignmentsWith0() throws Exception {
-        final Mutant mutant = getFirstMutant(HasLocalVariableWithTwoAssignments.class);
-        assertMutantCallableReturns(new HasLocalVariableWithTwoAssignments(), mutant, 0);
+    public void shouldReplaceSecondIntAssignmentsWith0() throws Exception {
+        final Mutant mutant = getNthMutant(HasLocalVariableWithSecondAssignment.class, 1);
+        assertMutantCallableReturns(new HasLocalVariableWithSecondAssignment(), mutant, 0);
+    }
+
+    private static class HasLocalAssignmentToMethodReturnValue implements Callable<Object> {
+
+        @Override
+        public Integer call() throws Exception {
+            int a = this.get10();
+            return a;
+        }
+
+        private int get10() {
+            return 10;
+        }
+    }
+
+    @Test
+    public void shouldReplaceAssignmentToMethodResultWith0() throws Exception {
+        final Mutant mutant = getFirstMutant(HasLocalAssignmentToMethodReturnValue.class);
+        assertMutantCallableReturns(new HasLocalAssignmentToMethodReturnValue(), mutant, 0);
+    }
+
+    private static class HasLocalDouble implements Callable<Object> {
+        @Override
+        public Double call() throws Exception {
+            double a = 1D;
+            return a;
+        }
+    }
+
+    @Test
+    public void shouldReplaceDoubleAssignmentWith0() throws Exception {
+        final Mutant mutant = getFirstMutant(HasLocalDouble.class);
+        assertMutantCallableReturns(new HasLocalDouble(), mutant, 0D);
+    }
+
+    private static class HasLocalLong implements Callable<Object> {
+        @Override
+        public Long call() throws Exception {
+            long a = 1L;
+            return a;
+        }
+    }
+
+    @Test
+    public void shouldReplaceLongAssignmentWith0() throws Exception {
+        final Mutant mutant = getFirstMutant(HasLocalLong.class);
+        assertMutantCallableReturns(new HasLocalLong(), mutant, 0L);
+    }
+
+    private static class HasLocalString implements Callable<Object> {
+        @Override
+        public String call() throws Exception {
+            String a = "a";
+            return a;
+        }
+    }
+
+    @Test
+    public void shouldReplaceStringAssignmentWithNull() throws Exception {
+        final Mutant mutant = getFirstMutant(HasLocalString.class);
+        assertMutantCallableReturns(new HasLocalString(), mutant, null);
     }
 }
